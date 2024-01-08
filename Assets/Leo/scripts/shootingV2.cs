fc7cc200479e51f74438719070193ca8d1981384
Leo Mauya <1717854@mukilteo.wednet.edu>
2024-01-08 13:17:40 -0800

    pooop.

--- Assets/Leo/scripts/shootingV2.cs
+++ Assets/Leo/scripts/shootingV2.cs
@@ -2,28 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shooting : MonoBehaviour
+public class shootingV2 : MonoBehaviour
 {
-
     private float x;
     public Rigidbody2D rb;
     public float speed;
     private bool moving = false;
     private Vector2 screenBounds;
     public object bulletPrefab;
-   
-
 
 
     // Update is called once per frame
     void Update()
     {
         rb = GetComponent<Rigidbody2D>();
-      
+
 
         x = transform.position.x;
 
-        if (Input.GetMouseButtonUp(0) == true )
+        if (Input.GetMouseButton(0) == true)
         {
             GetComponent<SpriteRenderer>().enabled = true;
             moving = true;
@@ -33,11 +30,9 @@ public class shooting : MonoBehaviour
         {
             x += 7 * Time.deltaTime * speed;
 
-         transform.position = new Vector2(x, transform.position.y);
+            transform.position = new Vector2(x, transform.position.y);
 
         }
 
-        
-        
     }
 }
--- Assets/Leo/scripts/switchbullets.cs
+++ Assets/Leo/scripts/switchbullets.cs
@@ -6,11 +6,20 @@ public class switchbullets : MonoBehaviour
 {
     public MonoBehaviour[] Toggle;
 
+    private int num1 = 1;
+    private int num2 = 2;
+    private int num3 = 3;
 
 
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(num1.ToString()))
+        {
+            
+        }
+
+       
+
     }
 }
