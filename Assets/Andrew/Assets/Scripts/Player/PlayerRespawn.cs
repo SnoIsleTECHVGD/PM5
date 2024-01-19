f4ee38d22ef81d0918383f26dc03ab28fa2fcc6a
Flores <1553135@mukilteo.wednet.edu>
2024-01-19 11:55:29 -0800

    fixed error in code

--- Assets/Andrew/Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Andrew/Assets/Scripts/Player/PlayerRespawn.cs
@@ -27,7 +27,7 @@ public class PlayerRespawn : MonoBehaviour
         if(collision.transform.tag == "Checkpoint")
         {
           currentCheckpoint = collision.transform;
-            SoundManager.instance.PlayerSound(checkpointSound);
+            
             collision.GetComponent<Collider2D>().enabled = false;
             collision.GetComponent<Animator>().SetTrigger("appear");
         }
